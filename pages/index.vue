f91d80b8db5bf480ab908853e0e68da83f9dfa8e
Severin Rosner <einsevspielt@gmail.com>
2024-10-08 12:32:20 +0200

    feat: init project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex justify-center h-full mt-32">
+  <h1 class="text-5xl font-black">GuideIT</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
