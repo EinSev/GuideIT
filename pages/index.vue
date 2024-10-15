46d9efeb3b2c9b9dd184d73452a596b8efd119b2
Severin Rosner <einsevspielt@gmail.com>
2024-10-15 11:10:00 +0200

    GI-18: feat: add icon module

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-<div class="flex justify-center h-full mt-32">
+<div class="flex justify-center items-center h-full mt-32">
   <h1 class="text-5xl font-black">GuideIT</h1>
+  <Icon name="material-symbols:forward-rounded" class="text-5xl"/>
 </div>
 </template>
 
