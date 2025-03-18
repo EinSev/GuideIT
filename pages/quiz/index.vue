28591077be408e4c1e11c33b66751493f0782c1b
Severin Rosner <einsevspielt@gmail.com>
2025-03-18 10:29:47 +0100

    GI-32: feat: responsive start quiz page

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -89,51 +89,52 @@ function reset() {
 </script>
 
 <template>
-  <div>
-    <div class="flex flex-col h-screen">
-      <div id="nav" class="flex flex-col justify-center items-center mt-5">
-        <div class="flex justify-end lg:justify-center w-full mr-5 md:mr-10 lg:mr-0">
-          <div class="hidden lg:flex">
-            <Menubar :model="items"
-              class="!rounded-3xl !bg-white-default !py-1 !px-2 !border-guideit-default !font-league-spartan !z-50 !shadow-guideit-200 !shadow-md"
-              :pt="{ item: '!focus:bg-white !active:bg-white-default !bg-none', itemContent: '!focus:bg-white-default !active:bg-white-default !bg-white-default', submenu: '!focus:bg-white-default !active:bg-white-default !bg-white-default !group-hover:underline !border-medt-default !z-50', itemLabel: '!text-black-default !text-lg group-hover:underline', itemLink: '!p-0 !py-1 !px-3'}" />
-          </div>
-          <NavDrawer />
+  <div class="flex flex-col h-screen mx-[5vw] sm:mx-[12vw] md:mx-0">
+    <div class="flex justify-between lg:justify-center w-full mt-5">
+      <NuxtLink to="/" class="lg:hidden">
+        <SvgoLogo class="w-32" :fontControlled="false" />
+      </NuxtLink>
+      <div>
+        <div class="hidden lg:flex">
+          <Menubar :model="items"
+            class="!rounded-3xl !bg-white-default !py-1 !px-2 !border-guideit-default !font-league-spartan !z-50 !shadow-guideit-200 !shadow-md"
+            :pt="{ item: '!focus:bg-white !active:bg-white-default !bg-none', itemContent: '!focus:bg-white-default !active:bg-white-default !bg-white-default', submenu: '!focus:bg-white-default !active:bg-white-default !bg-white-default !group-hover:underline !border-guideit-default !z-50', itemLabel: '!text-black-default !text-lg group-hover:underline', itemLink: '!p-0 !py-1 !px-3' }" />
         </div>
+        <NavDrawer />
       </div>
-      <div id="content relative">
-        <h1 id="heading"
-          class="mt-8 lg:mt-12 text-heading-small text-center lg:text-heading font-semibold text-guideit-default">
-          Mach das Quiz!
-        </h1>
-        <div class="px-20 mt-8 text-text leading-9 flex">
-          <div>
-            <p>Netzwerktechnik? Mediendesign und Medienproduktion? Oder Web- & App-Entwicklung? Mach das GuideIT-Quiz
-              und
-              finde es heraus
-            </p>
-            <p class="mt-5">
-              <b>Achtung:</b> Das Quiz gibt dir nur eine Orientierung für deine Entscheidung und soll
-              dich
-              unterstützen - denn niemand außer du selbst kann diese Entscheidung treffen!
-            </p>
-            <div class="flex justify-center mt-16">
-              <button @click="reset"
-                class="pt-2.5 pb-1 bg-guideit-50 px-16 text-sub-heading rounded-lg border-2 border-guideit-400
+    </div>
+    <div id="content relative">
+      <h1 id="heading"
+        class="mt-5 lg:mt-12 text-heading-small text-center lg:text-heading font-semibold text-guideit-default">
+        Mach das Quiz!
+      </h1>
+      <div class="lg:px-20 mt-2 lg:mt-8 text-text leading-9 flex flex-col w-full lg:flex-row">
+        <div>
+          <p>
+            Netzwerktechnik? Mediendesign und Medienproduktion? Oder Web- & App-Entwicklung? Mach das GuideIT-Quiz
+            und
+            finde es heraus
+          </p>
+          <p class="mt-5">
+            <b>Achtung:</b> Das Quiz gibt dir nur eine Orientierung für deine Entscheidung und soll
+            dich
+            unterstützen - denn niemand außer du selbst kann diese Entscheidung treffen!
+          </p>
+          <div class="flex max-lg:ml-8 mt-5 lg:justify-center lg:mt-16">
+            <button @click="reset" class="pt-0.5 lg:pt-2.5 lg:pb-1 bg-guideit-50 px-8 lg:px-16 text-text lg:text-sub-heading rounded-lg border-[3px] border-guideit-400
                 hover:bg-guideit-default hover:border-guideit-400 hover:text-guideit-50 text-guideit-800">
-                Loslegen!
-              </button>
-            </div>
+              Loslegen!
+            </button>
           </div>
-          <SvgoQuiz class="w-[70rem] -mt-20" :fontControlled="false" filled />
         </div>
+        <SvgoQuiz class="w-full lg:w-[70rem] mt-10 lg:-mt-20" :fontControlled="false" filled />
       </div>
-      <div class="flex mt-auto text-black-300 justify-between px-2 items-end">
-        <NuxtLink to="/impressum" class="text-black-600 ml-2 mb-1">Impressum</NuxtLink>
-        <NuxtLink to="https://www.htlrennweg.at" rel="noopener noreferrer" class="w-20 lg:w-32" target="_blank"
-          external><img src="/assets/img/htl3r_logo_slogan_transparent.png" alt="Logo der HTL Rennweg" />
-        </NuxtLink>
-      </div>
+    </div>
+    <div class="flex mt-auto text-black-300 justify-between px-2 items-end">
+      <NuxtLink to="/impressum" class="text-black-600 ml-2 mb-1">Impressum</NuxtLink>
+      <NuxtLink to="https://www.htlrennweg.at" rel="noopener noreferrer" class="w-20 lg:w-32" target="_blank" external>
+        <img src="/assets/img/htl3r_logo_slogan_transparent.png" alt="Logo der HTL Rennweg" />
+      </NuxtLink>
     </div>
   </div>
 </template>
